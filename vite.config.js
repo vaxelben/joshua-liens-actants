684df34dc995eea99b58374da3f39ac9c3ef2802
vaxelben <benjamin@datawrap.fr>
2025-02-19 22:20:55 +0100

    still patches

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/joshua-liens-actants/',  // Correction du nom du repo
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['tweakpane', 'd3']
+        }
+      }
+    }
+  }
 }) 
